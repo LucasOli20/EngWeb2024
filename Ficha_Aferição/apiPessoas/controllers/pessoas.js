45ebfab3e16619a98cfe1b7d93e112d10aa74fb0
LucasOli20 <a98695@alunos.uminho.pt>
2024-04-09 20:20:38 +0100

    exec 5 feito

--- Ficha_Aferição/apiPessoas/controllers/pessoas.js
+++ Ficha_Aferição/apiPessoas/controllers/pessoas.js
@@ -6,12 +6,18 @@ var pessoa = require("../model/pessoas")
 module.exports.list = () => {
     return pessoa
         .find()
-        .sort({nome:1})
+        .sort({nome : 1})
+        .exec()
+}
+
+module.exports.findByBI = BI => {
+    return pessoa
+        .findOne({BI : BI})
         .exec()
 }
 
 module.exports.insert = (pessoa) => {
-    if((pessoa.find({BI}).exec()).length != 1){
+    if((pessoa.find({BI : pessoa.BI}).exec()).length != 1){
         var newPessoa = new Pessoa(pessoa)
         return newPessoa.save()
     }
@@ -19,13 +25,37 @@ module.exports.insert = (pessoa) => {
 
 module.exports.remove = (Bi) => {
     pessoa
-        .find({Bi})
+        .find({Bi : Bi})
         .deleteOne()
         .exec()
 }
 
 module.exports.update = (Bi, pessoa) => {
     return pessoa
-        .findByIdAndUpdate(Bi, pessoa, {new : true})
+        .findByBIAndUpdate(Bi, pessoa, {new : true})
         .exec()
 }
+
+// module.exports.listModalidades = () => {
+//     return pessoa
+//         .aggregate([
+//             { $unwind: "$pessoas" },
+//             { $unwind: "$pessoas.desportos"},
+//             { $group: {_id: "$pessoas.desportos"} },
+//             { $sort: {_id: 1}}
+//         ])
+//         .exec()
+// }
+
+module.exports.listModalidades = () => {
+    return pessoa.aggregate([
+        { $unwind: "$pessoas" }, 
+        { $unwind: "$pessoas.desportos" },
+        { $group: { _id: "$pessoas.desportos" } },
+        { $sort: { _id: 1 } },
+        { $project: { _id: 0, nome: "$_id" } },
+        { $group: { _id: null, desportos: { $push: "$nome" } } }
+    ]).exec().then(result => {
+        return result.length > 0 ? result[0].desportos : [];
+    });
+};
--- Ficha_Aferição/apiPessoas/routes/pessoas.js
+++ Ficha_Aferição/apiPessoas/routes/pessoas.js
@@ -6,24 +6,30 @@ router.get('/', function(req, res, next) {
     Pessoa.list()
       .then(data => res.jsonp(data))
       .catch(erro => res.jsonp(erro))
-  });
-  
-  router.post('/', function(req, res, next) {
-    Pessoa.insert(req.body)
+});
+
+router.get('/modalidades', function(req, res, next){
+    Pessoa.listModalidades()
       .then(data => res.jsonp(data))
       .catch(erro => res.jsonp(erro))
-  });
-  
-  router.put('/:BI', function(req, res, next) {
-    Pessoa.update(req.params.BI, req.body)
-      .then(data => res.jsonp(data))
-      .catch(erro => res.jsonp(erro))
-  });
-  
-  router.post('/:BI', function(req, res, next) {
-    Pessoa.delete(req.params.BI)
-      .then(console.log("Deleted " + req.params.id))
-      .catch(erro => res.jsonp(erro))
-  });
+});
+
+router.post('/', function(req, res, next) {
+  Pessoa.insert(req.body)
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
+});
+
+router.put('/:BI', function(req, res, next) {
+  Pessoa.update(req.params.BI, req.body)
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
+});
+
+router.post('/:BI', function(req, res, next) {
+  Pessoa.delete(req.params.BI)
+    .then(console.log("Deleted " + req.params.id))
+    .catch(erro => res.jsonp(erro))
+});
   
   module.exports = router;
